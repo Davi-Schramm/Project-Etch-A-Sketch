2481911ee5f006ecedd562335a15f6c0b262bb9b
Davi-Schramm <davi.s.schramm@gmail.com>
2022-08-14 13:09:14 -0300

    added the backtrace of draws

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ for(let i = 0;i<16;i++){
 // i são as linhas
 // j são os blocos
 
-
 for(let i = 0;i<16;i++){
     for(let j = 0;j<16;j++){
         matrix[i][j] = document.createElement('div');
         matrix[i][j].classList.add('flexdraw');
+        matrix[i][j].onmouseover = function(){
+            this.style.cssText += "background-color:black;";
+        };
         lines[i].appendChild(matrix[i][j]);
     }
     
